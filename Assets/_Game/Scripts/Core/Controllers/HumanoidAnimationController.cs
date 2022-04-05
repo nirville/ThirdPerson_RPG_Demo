5d001c12ed4e3a6074b06bedb62230e2e7a3dcad
Niraj Sharma <nirajhimself@gmail.com>
2022-04-05 17:42:52 +0530

    add player animations & environment

---
+++ Assets/_Game/Scripts/Core/Controllers/HumanoidAnimationController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class HumanoidAnimationController : MonoBehaviour
+{
+    Animator charAnimator;
+
+    void Start()
+    {
+        charAnimator = GetComponent<Animator>();
+        AnimationEvents.current.onWalkStart += OnWalk;
+        AnimationEvents.current.onWalkEnd += OnIdle;
+    }
+
+    void OnWalk()
+    {
+        charAnimator.SetTrigger("runTrigger");
+    }
+
+    void OnJump()
+    {
+
+    }
+
+    void OnIdle()
+    {
+        charAnimator.SetTrigger("idleTrigger");
+    }
+
+    void OnDisable()
+    {
+        AnimationEvents.current.onWalkStart -= OnWalk;
+        AnimationEvents.current.onWalkEnd -= OnIdle;
+    }
+}
---
+++ Assets/_Game/Scripts/Core/Events/AnimationEvents.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+public class AnimationEvents : MonoBehaviour
+{
+    public static AnimationEvents current;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    public event Action onWalkStart;
+    public event Action onWalkEnd;
+
+    public void WalkForward()
+    {
+        if(onWalkStart != null)
+            onWalkStart();
+    }
+
+     public void Idle()
+    {
+        if(onWalkEnd != null)
+            onWalkEnd();
+    }
+}
